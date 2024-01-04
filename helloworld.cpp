7cd3a5a1668968d924309b2b5ea4bfbaf8050801
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-04 11:25:44 -0600

    output to terminal test

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,4 +1,16 @@
+/*iostream is a header file that contains
+functions for basic input/output operations*/
 #include <iostream>
+int main(){
+    //standard character output
+    //std::cout << "I like tonkatsu ramen";
+    std::cout << "I like tonkatsu ramen" << std::endl;
+    //if you need the next line of text to be on the next line
+    std::cout << "But aka-miso is also delicious"  << std::endl;
+
+//if 1 is returned, it means there is a problem
+    return 0;
+}
 
 
 
