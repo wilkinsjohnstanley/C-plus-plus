2acbac2ad68143d5a58db034d81f7c03cc488b29
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-04 12:09:30 -0600

    using keyword clarification

--- 4-namespace.cpp
+++ 4-namespace.cpp
@@ -6,6 +6,11 @@ namespace alternateUniverse{
 }
 
 int main(){
+    /*
+    if you don't want to use the prefix, you can use the using keyword
+    using namespace alternateUniverse;
+    then it is assumed that the variable is the alternateUniverse variable
+    */
     int doppleganger = 2;
     std::cout << doppleganger << '\n';
 //use the alternate universe doppleganger
