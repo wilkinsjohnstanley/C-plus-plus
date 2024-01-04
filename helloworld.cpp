46fc893f093f711a25420fcc5e93310775f7f4c3
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-04 11:29:53 -0600

    done

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,10 +3,14 @@ functions for basic input/output operations*/
 #include <iostream>
 int main(){
     //standard character output
+ 
     //std::cout << "I like tonkatsu ramen";
     std::cout << "I like tonkatsu ramen" << std::endl;
     //if you need the next line of text to be on the next line
     std::cout << "But aka-miso is also delicious"  << std::endl;
+
+
+
 
 //if 1 is returned, it means there is a problem
     return 0;
