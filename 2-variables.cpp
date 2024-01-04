fe9662603f81f126901173e632f2de958a458069
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-04 11:46:10 -0600

    more variables were tested

--- 2-variables.cpp
+++ 2-variables.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+//use CTRL + K + CTRL + T to change colors in VS Code
 int main(){
 
     int x; //declaration
     x=5; //assignment
-
-    std::cout << x;
+    int y  = 6;
+    int sum = x+y;
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+//double (number including a decimal)
+    double price = 10.99;
+    double GPA = 3.7;
+    double temperature = 25.1;
+    std::cout << price << '\n';
+//char
+    char grade = 'A';
+    char initial = 'J';
+    std::cout << grade << '\n';
+    char currency = '$';
+//boolean
+    bool student = true;
+    bool power = true;
+    bool inStock = false;
+//strings (an object that represents a sequence of text)
+//strings are provided from the standard namespace
+    std::string name = "John";
+    std::cout << name;
 
     return 0;
 }
