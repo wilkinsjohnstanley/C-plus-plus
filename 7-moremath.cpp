bf13b8bb86c6cb6be5931bedb26a342809449f89
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-04 21:47:44 -0600

    product, sum, quotient, and difference

---
+++ 7-moremath.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+int one = 1;
+int two = 2;
+
+int product = one*two;
+int sum = one+two;
+int quotient = one/two;
+int difference = one-two;
+    std::cout << product << '\n';
+    std::cout << sum << '\n';
+    std::cout << difference << '\n';
+    std::cout << quotient << '\n';
+
+
+    return 0;
+}
