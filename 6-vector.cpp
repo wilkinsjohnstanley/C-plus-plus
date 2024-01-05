bca2ebcb21d907087916bb51c7dd5c99de1f9353
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-04 21:17:21 -0600

    using is simpler than typedef!

--- 6-vector.cpp
+++ 6-vector.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <vector>
 
-typedef std::vector<std::pair<std::string, int>> pairlist_t;
-typedef std::string text_t;
-typedef int number_t;
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+// typedef std::string text_t;
+// typedef int number_t;
+
+//You can see that 'using' is simpler!
+using text_t = std::string;
+using number_t = int;
 
 int main(){
     text_t firstName = "Bro";
